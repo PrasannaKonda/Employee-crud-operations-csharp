7191c1e2908639b93102b56d427fe1f9c95a6070
PrasannaKonda <kondalakshmiprasanna1804@gmail.com>
2025-10-03 22:28:32 +0530

    View Bag

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,21 +19,9 @@ namespace MyFirstApp.Controllers
         public ViewResult Details()
         {
             Employee model = _employeeRepository.GetEmployee(1);
-            ViewData["Employee"] = model;
-            ViewData["PageTitle"] = "Employee Details";
+            ViewBag.Employee = model;
+            ViewBag.PageTitle = "Employee Details";
             return View(model);
         }
-
-        //public JsonResult Details()
-        //{
-        //    Employee model = _employeeRepository.GetEmployee(1);
-        //    return Json(model);
-        //}
-
-        //public ObjectResult Details()
-        //{
-        //    Employee model = _employeeRepository.GetEmployee(1);
-        //    return new ObjectResult(model);
-        //}
     }
 }
